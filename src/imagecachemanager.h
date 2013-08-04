f96c655337373c5290b9fe9d458a7fcc32df4a09
Chris Pilkington <chris.pilkington@gmail.com>
2013-08-04 22:49:22 +1000

    Cache manager can have static functions.

--- src/imagecachemanager.h
+++ src/imagecachemanager.h
@@ -18,9 +18,9 @@ namespace diesel
     static bool IsUFRawBatchInstalled();
     static bool IsConvertInstalled();
 
-    string_t GetOrCreateDNGForRawFile(const string_t& sRawFilePath);
-    string_t GetOrCreateThumbnailForDNGFile(const string_t& sDNGFilePath, IMAGE_SIZE imageSize);
-    string_t GetOrCreateThumbnailForImageFile(const string_t& sImageFilePath, IMAGE_SIZE imageSize);
+    static string_t GetOrCreateDNGForRawFile(const string_t& sRawFilePath);
+    static string_t GetOrCreateThumbnailForDNGFile(const string_t& sDNGFilePath, IMAGE_SIZE imageSize);
+    static string_t GetOrCreateThumbnailForImageFile(const string_t& sImageFilePath, IMAGE_SIZE imageSize);
 
   private:
     static string_t GetCacheFolderPath();
